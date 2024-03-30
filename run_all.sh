dfee62032baec6b0f1dad1a9e453e29fa7668ed1
Stefan F. Schouten <sfschouten@gmail.com>
2024-03-30 13:00:17 +0100

    cleanup

--- run_all.sh
+++ run_all.sh
@@ -7,17 +7,17 @@ tango run config/exp1.jsonnet --name "exp1_entbank_$DATE" --ext-var dataset=entb
 
 
 # experiment 2
-tango run config/exp2.jsonnet --name "exp2_llama2-7b_snli_$DATE" --ext-var dataset=snli --ext-var model=llama2-7b
-tango run config/exp2.jsonnet --name "exp2_llama2-7b_entbank_$DATE" --ext-var dataset=entbank --ext-var model=llama2-7b
+#tango run config/exp2.jsonnet --name "exp2_llama2-7b_snli_$DATE" --ext-var dataset=snli --ext-var model=llama2-7b
+#tango run config/exp2.jsonnet --name "exp2_llama2-7b_entbank_$DATE" --ext-var dataset=entbank --ext-var model=llama2-7b
 
-tango run config/exp2.jsonnet --name "exp2_llama2-13b_snli_$DATE" --ext-var dataset=snli --ext-var model=llama2-13b
+#tango run config/exp2.jsonnet --name "exp2_llama2-13b_snli_$DATE" --ext-var dataset=snli --ext-var model=llama2-13b
 tango run config/exp2.jsonnet --name "exp2_llama2-13b_entbank_$DATE" --ext-var dataset=entbank --ext-var model=llama2-13b
-
-tango run config/exp2.jsonnet --name "exp2_olmo-7b_snli_$DATE" --ext-var dataset=snli --ext-var model=olmo-7b
-tango run config/exp2.jsonnet --name "exp2_olmo-7b_entbank_$DATE" --ext-var dataset=entbank --ext-var model=olmo-7b
-
-tango run config/exp2.jsonnet --name "exp2_olmo-7b-instruct_snli_$DATE" --ext-var dataset=snli --ext-var model=olmo-7b-instruct
-tango run config/exp2.jsonnet --name "exp2_olmo-7b-instruct_entbank_$DATE" --ext-var dataset=entbank --ext-var model=olmo-7b-instruct
+#
+#tango run config/exp2.jsonnet --name "exp2_olmo-7b_snli_$DATE" --ext-var dataset=snli --ext-var model=olmo-7b
+#tango run config/exp2.jsonnet --name "exp2_olmo-7b_entbank_$DATE" --ext-var dataset=entbank --ext-var model=olmo-7b
+#
+#tango run config/exp2.jsonnet --name "exp2_olmo-7b-instruct_snli_$DATE" --ext-var dataset=snli --ext-var model=olmo-7b-instruct
+#tango run config/exp2.jsonnet --name "exp2_olmo-7b-instruct_entbank_$DATE" --ext-var dataset=entbank --ext-var model=olmo-7b-instruct
 
 
 
