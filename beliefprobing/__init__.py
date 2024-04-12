66c9214a0f0d6c65081a8c261df2f4ca1a87df5f
Stefan F. Schouten <sfschouten@gmail.com>
2024-04-12 21:47:10 +0200

    beliefprobing package was moved to own repository

--- beliefprobing/__init__.py
+++
@@ -1,5 +0,0 @@
-from beliefprobing.generate import Generate
-from beliefprobing.intervene import IntervenedGenerate
-from beliefprobing.integrations import *
-
-from beliefprobing.probes.beliefprobe import BeliefProbe
--- beliefprobing/datasets/got_sp_en_trans/templates.libsonnet
+++
@@ -1,8 +0,0 @@
-{
-    'original': {
-        type: 'custom_template',
-        name: 'original',
-        answer_choices: ' ||| ',
-        jinja: '{{ start }} ||| {{ [neg_end, pos_end][label] }}'
-    }
-}
